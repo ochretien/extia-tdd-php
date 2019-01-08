a8817dccf0ab6f0113396f3a7968b4dccf8026b8
Olivier Chretien <ochretien@gmail.com>
2019-01-08 14:40:05 +0100

    Initial commit

---
+++ src/Service/Impl/CommandeServiceImpl.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Service\Impl;
+
+use App\Service\CommandeServiceInterface;
+
+class CommandeServiceImpl implements CommandeServiceInterface
+{}
---
+++ tests/Service/Impl/CommandeServiceImplTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Service\Impl;
+
+use PHPUnit\Framework\TestCase;
+
+class CommandeServiceImplTest extends TestCase
+{
+    public function testVoid()
+    {
+        $this->assertNotNull(null);
+    }
+}
